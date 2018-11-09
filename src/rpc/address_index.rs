934b220c0d2e2d4986d7a9c0611ac57ca6e2880c
Jorian <jorian@outlook.com>
2018-11-09 10:02:05 +0100

    added addressdelta

--- src/rpc/address_index.rs
+++ src/rpc/address_index.rs
@@ -1,5 +1,72 @@
 use Address;
 
+#[derive(Debug, Deserialize)]
+pub struct AddressBalance {
+    pub balance: u64,
+    pub received: u64,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct AddressDeltas(Vec<AddressDelta>);
+
+
+#[derive(Debug, Deserialize)]
+pub struct AddressDelta {
+    pub satoshis: i64,
+    pub txid: String,
+    pub index: u32,
+    pub blockindex: u64,
+    pub height: u64,
+    pub address: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct AddressMempool(Vec<AddressMempoolDelta>);
+
+#[derive(Debug, Deserialize)]
+pub struct AddressMempoolDelta {
+    address: String,
+    txid: String,
+    index: u32,
+    satoshis: i64,
+    timestamp: u64,
+    prevtxid: String,
+    prevout: u32,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct AddressTxIDs(Vec<String>); // todo: a vec with txids, needs work.
+
+#[derive(Debug, Deserialize)]
+pub struct AddressUtxos {
+    pub vec: Vec<AddressUtxo>
+}
+
+#[derive(Debug, Deserialize)]
+pub struct AddressUtxo {
+    pub address: String,
+    pub txid: String,
+    pub height: u64,
+    #[serde(rename = "outputIndex")]
+    pub output_index: u32,
+    pub script: String,
+    pub satoshis: u64 // output always positive, no signing needed
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Snapshot {
+    pub addresses: Vec<Address>,
+    pub total: f64,
+    pub average: f64,
+    pub utxos: u64,
+    pub total_addresses: u64,
+    pub start_height: u64,
+    pub ending_height: u64,
+    pub start_time: u64,
+    pub end_time: u64,
+}
+
+
 /*
 //    getaddressbalance
 //    getaddressdeltas
@@ -82,70 +149,3 @@ getsnapshot (top)
   "end_time": 1531982752    (number) Unix epoch time snapshot finished
 }
 */
-
-
-#[derive(Debug, Deserialize)]
-pub struct AddressBalance {
-    pub balance: u64,
-    pub received: u64,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct AddressDeltas {
-    pub vec: Vec<AddressDelta>,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct AddressDelta {
-    pub satoshis: i64,
-    pub txid: String,
-    pub index: u32,
-    pub blockindex: u64,
-    pub height: u64,
-    pub address: String,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct AddressMempool(Vec<AddressMempoolDelta>);
-
-#[derive(Debug, Deserialize)]
-pub struct AddressMempoolDelta {
-    address: String,
-    txid: String,
-    index: u32,
-    satoshis: i64,
-    timestamp: u64,
-    prevtxid: String,
-    prevout: u32,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct AddressTxIDs(Vec<String>); // todo: a vec with txids, needs work.
-
-#[derive(Debug, Deserialize)]
-pub struct AddressUtxos {
-    pub vec: Vec<AddressUtxo>
-}
-
-#[derive(Debug, Deserialize)]
-pub struct AddressUtxo {
-    pub address: String,
-    pub txid: String,
-    pub height: u64,
-    pub outputIndex: u32,
-    pub script: String,
-    pub satoshis: u64 // output always positive, no signing needed
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Snapshot {
-    pub addresses: Vec<Address>,
-    pub total: f64,
-    pub average: f64,
-    pub utxos: u64,
-    pub total_addresses: u64,
-    pub start_height: u64,
-    pub ending_height: u64,
-    pub start_time: u64,
-    pub end_time: u64,
-}
