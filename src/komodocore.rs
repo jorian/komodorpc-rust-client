1ee9e44ae05d970b85e57f8fc70768ebcacc5708
Jorian <jorian@outlook.com>
2018-11-11 20:18:46 +0100

    start with returning Result types

--- src/komodocore.rs
+++ src/komodocore.rs
@@ -15,7 +15,8 @@ use dirs;
 
 use arguments::AddressList;
 use std::collections::HashMap;
-use std::error::Error;
+
+use std::io::Error;
 
 pub struct Client {
     client: RpcClient,
@@ -25,28 +26,24 @@ pub struct Client {
 impl Client {
     /// Constructs a new `Client` that talks to the Komodo main chain. It assumes Komodo has
     /// been installed, since it fetches the needed RPC authentication parameters from the config file.
-    pub fn new_komodo_client() -> Self {
-
-        let config = Config::get_for(Chain::KMD);
-
+    pub fn new_komodo_client() -> Result<Self, Error> {
+        let config = Config::get_for(Chain::KMD)?;
         let rpc_client = Client::construct_rpc_client(&config);
 
-        Client {
+        Ok(Client {
             client: rpc_client,
             config,
-        }
+        })
     }
 
-    pub fn new_assetchain_client(ac: Chain) -> Self {
-
-        let config = Config::get_for(ac);
-
+    pub fn new_assetchain_client(ac: Chain) -> Result<Self, Error> {
+        let config = Config::get_for(ac)?;
         let rpc_client = Client::construct_rpc_client(&config);
 
-        Client {
+        Ok(Client {
             client: rpc_client,
             config,
-        }
+        })
     }
 
     fn construct_rpc_client(config: &Config) -> RpcClient {
@@ -83,6 +80,76 @@ impl Client {
             _ => return result
         }
         self.client.send(request)
+    }
+}
+
+struct Config {
+    rpc_user: String,
+    rpc_password: String,
+    rpc_port: u16,
+}
+
+impl Config {
+    pub fn get_for(chain: Chain) -> Result<Self, Error> {
+        let config_file_path;
+
+        if let Some(mut path) = dirs::home_dir() {
+            // todo: location of komodo files differ for each platform
+            path.push(".komodo/");
+
+            match chain {
+                Chain::KMD => {
+                    path.push("komodo.conf");
+                }
+                _ => {
+                    path.push(chain.to_string());
+                    path.push(format!("{}.conf", chain.to_string()));
+                }
+            }
+
+            config_file_path = path.to_str().unwrap().to_owned();
+        } else {
+            // todo: what happens when no home dir is found
+            config_file_path = String::new();
+        }
+
+        let contents = fs::read_to_string(config_file_path)?;
+//        let contents = fs::read_to_string(config_file_path).expect("unable to open config file");
+
+        let map: HashMap<String, String> = contents.as_str()
+            .split('\n')
+            .map(|line| line.splitn(2, '=').collect::<Vec<&str>>())
+            .filter(|vec| vec.len() == 2)
+            .map(|vec| (
+                vec[0].to_string(),
+                vec[1].to_string()
+            ))
+            .collect::<HashMap<String, String>>();
+
+        // todo this shouldn't panic:
+
+//        let mut _rpc_user;
+//        let mut _rpc_password;
+//        let mut _rpc_port;
+//
+//        match map.get("rpcuser") {
+//            Ok(result) => _rpc_user = result,
+//            Err(e) => return Result::Err()
+//        }
+
+        let _rpc_user = map.get("rpcuser").expect("no rpcuser in config file");
+        let _rpc_password = map.get("rpcpassword").expect("no rpcpassword in config file");
+        let _rpc_port =
+            match chain {
+                Chain::KMD => "7771", // todo: KMD doesn't put rpcport in conf file at install
+                _ => map.get("rpcport").expect("no rpcport in config file"),
+            };
+
+        Ok(Config {
+            rpc_user:       _rpc_user.to_owned(),
+            rpc_password:   _rpc_password.to_owned(),
+            rpc_port:       _rpc_port.parse::<u16>().unwrap()
+        })
     }
 }
 
@@ -204,74 +271,5 @@ impl KomodoRpcApi for Client {
             "777",
             "getsnapshot"
         ))
-    }
-}
-
-struct Config {
-    rpc_user: String,
-    rpc_password: String,
-    rpc_port: u16,
-}
-
-impl Config {
-    pub fn get_for(chain: Chain) -> Self {
-        let config_file_path;
-
-        if let Some(mut path) = dirs::home_dir() {
-            // todo: location of komodo files differ for each platform
-            path.push(".komodo/");
-
-            match chain {
-                Chain::KMD => {
-                    path.push("komodo.conf");
-                }
-                _ => {
-                    path.push(chain.to_string());
-                    path.push(format!("{}.conf", chain.to_string()));
-                }
-            }
-
-            config_file_path = path.to_str().unwrap().to_owned();
-        } else {
-            // todo: what happens when no home dir is found
-            config_file_path = String::new();
-        }
-
-        let contents = fs::read_to_string(config_file_path).expect("unable to open config file");
-
-        let map: HashMap<String, String> = contents.as_str()
-            .split('\n')
-            .map(|line| line.splitn(2, '=').collect::<Vec<&str>>())
-            .filter(|vec| vec.len() == 2)
-            .map(|vec| (
-                vec[0].to_string(),
-                vec[1].to_string()
-            ))
-            .collect::<HashMap<String, String>>();
-
-        // todo this shouldn't panic:
-
-//        let mut _rpc_user;
-//        let mut _rpc_password;
-//        let mut _rpc_port;
-//
-//        match map.get("rpcuser") {
-//            Ok(result) => _rpc_user = result,
-//            Err(e) => return Result::Err()
-//        }
-
-        let _rpc_user = map.get("rpcuser").expect("no rpcuser in config file");
-        let _rpc_password = map.get("rpcpassword").expect("no rpcpassword in config file");
-        let _rpc_port =
-            match chain {
-                Chain::KMD => "7771", // todo: KMD doesn't put rpcport in conf file at install
-                _ => map.get("rpcport").expect("no rpcport in config file"),
-            };
-
-        Config {
-            rpc_user:       _rpc_user.to_owned(),
-            rpc_password:   _rpc_password.to_owned(),
-            rpc_port:       _rpc_port.parse::<u16>().unwrap()
-        }
     }
 }
