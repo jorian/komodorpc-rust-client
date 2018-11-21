c19bda15c582f5d6e7395ff43860b614f3a9e7c7
Jorian <jorian@outlook.com>
2018-11-21 19:59:23 +0100

    journey into error land

---
+++ src/error.rs
@@ -0,0 +1,49 @@
+use jsonrpc_client::{RpcError, ClientError};
+use core::fmt;
+use std::fmt::Formatter;
+use std::error::Error;
+
+#[derive(Debug)]
+enum ApiError {
+    RPC(RpcError),
+    Client(ClientError),
+    Other
+}
+
+impl fmt::Display for ApiError {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match *self {
+            ApiError::RPC(ref cause) => write!(f, "RPC error: {}", cause.message ),
+            ApiError::Client(ref cause) => write!(f, "Client error" ),
+            ApiError::Other => write!(f, "Unknown error")
+        }
+    }
+}
+
+impl Error for ApiError {
+    fn description(&self) -> &str {
+        match *self {
+            ApiError::RPC(ref cause) => cause.description(),
+            ApiError::Client(ref cause) => cause.description(),
+            ApiError::Other => "Unknown error!",
+        }
+    }
+
+    fn cause(&self) -> Option<&Error> {
+        match *self {
+            ApiError::RPC(ref cause) => Some(cause),
+            ApiError::Client(ref cause) => Some(cause),
+            ApiError::Other => None,
+        }
+    }
+}
+impl From<RpcError> for ApiError {
+    fn from(cause: RpcError) -> ApiError {
+        ApiError::RPC(cause)
+    }
+}
+impl From<ClientErrorError> for ApiError {
+    fn from(cause: ClientError) -> ApiError {
+        ApiError::Client(cause)
+    }
+}
